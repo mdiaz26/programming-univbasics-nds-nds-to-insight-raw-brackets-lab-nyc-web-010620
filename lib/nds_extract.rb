8d4cb27adb447caeae10b213ded06e846b98b29d
Michael Diaz <mike.diaz006@gmail.com>
2019-12-17 19:08:57 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -22,10 +22,11 @@ def directors_totals(nds)
   first_index = 0 
     while first_index < nds.length 
       name_of_director = nds[first_index][:name]
-      movie_number = 0 
+      movie_number = 0
+      total_amount = 0 
         while movie_number < nds[first_index][:movies].length
           movies_data = nds[first_index][:movies][movie_number]
-          total_amount = movies_data[:worldwide_gross]
+          total_amount += movies_data[:worldwide_gross]
           movie_number += 1
           total_hash[name_of_director] = total_amount
         end
